7b936aff105902227451454c2e262d7865f75c36
Rasha Moumneh <rasha@fastmail.com>
2020-09-01 18:12:00 -0400

    Create grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import produce from "immer";
+import "./App.css";
+
+const numRows = 50;
+const numColumns = 50;
 
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numColumns), () => 0));
+    }
+    return rows;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${numColumns}, 20px)`,
+      }}
+    >
+      {grid.map((rows, i) =>
+        rows.map((col, j) => (
+          <div
+            // click sets value to 1 (alive). Immer 'produce' allows us to mutate a copy of the grid
+            onClick={() => {
+              const newGrid = produce(grid, (gridCopy) => {
+                gridCopy[i][j] = 1;
+              });
+              setGrid(newGrid);
+            }}
+            key={`${i}-${j}`}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: grid[i][j] ? "pink" : undefined,
+              border: "solid 1px black",
+            }}
+          />
+        ))
+      )}
     </div>
   );
 }
